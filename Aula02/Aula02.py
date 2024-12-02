3c3b873ad0630d4c12539f3d8d825dbe05309b94
Isah2525 <116742255+Isah2525@users.noreply.github.com>
2024-12-02 10:00:23 -0300

    Criação de variáveis e print com concatenação

---
+++ Aula02/Aula02.py
@@ -0,0 +1,23 @@
+print("Ola mundo")
+
+
+a = 5
+b = 2
+resultado  = (a + b)
+#print (resultado)
+
+#declaração de variaveis
+nome_usuario = "isabella" #string
+idade_usuario = 19 #int
+altura = 1.56 #float 
+maior_de_idade = True #boolean
+
+
+#print(nome_usuario)
+#print(idade_usuario)
+#print(altura)
+#print(maior_de_idade)
+
+print("Seja bem vindo",nome_usuario, "sua idada é" ,idade_usuario,
+       "sua altura é" ,altura, "você é maior de idade ? ", maior_de_idade)
+
